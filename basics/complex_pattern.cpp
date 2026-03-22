a8479304af6a377146850becde0e168ec7e31961
nikhilyt01 <nikhil0808yt@gmail.com>
2026-03-22 23:52:35 +0530

    space+two traingle complex

---
+++ basics/complex_pattern.cpp
@@ -0,0 +1,32 @@
+// _ _ _ 1
+// _ _ 1 2 1
+// _ 1 2 3 2 1
+// 1 2 3 4 3 2 1
+#include <iostream>
+using namespace std;    
+int main(){
+    cout << "Enter the number of Rows: ";
+    int n;
+    cin >> n;
+    int row =1;
+    while(row<=n){
+        int spaces = n-row ;
+        while(spaces){
+            cout << "  ";
+            spaces--;
+        }
+        int  col=1;
+        while(col<=row){
+            cout << col<< " " ;
+            col++;
+        }
+        int start =row -1;
+        while(start){
+            cout << start<< " " ;
+            start--;
+        }
+        cout << endl;
+        row++;
+    }
+    
+}
