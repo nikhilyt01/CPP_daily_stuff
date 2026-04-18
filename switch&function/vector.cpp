5dc325ebaa0d7b96282e38108b7f5e4a99ff41fb
nikhilyt01 <nikhil0808yt@gmail.com>
2026-04-18 22:58:13 +0530

    vectors basics

---
+++ switch&function/vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(4);
+    v.push_back(3);
+    v.push_back(8);
+    v.pop_back();
+    sort(v.begin(),v.end());
+
+    for(int i=0;i<v.size();i++){
+        cout<< v[i] << endl;
+    }
+}
