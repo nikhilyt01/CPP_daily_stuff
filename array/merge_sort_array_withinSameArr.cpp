1d8ff81214273de43750dfe48b8309114bede439
nikhilyt01 <nikhil0808yt@gmail.com>
2026-06-24 14:04:45 +0530

    leetcode 88.merge Sorted

---
+++ array/merge_sort_array_withinSameArr.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // Pointers for the end of valid elements in nums1 and nums2
+        int i = m - 1;
+        int j = n - 1;
+        // Pointer for the last position in the combined nums1 array
+        int k = m + n - 1;
+        
+        // Merge from right to left
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        
+        // If there are remaining elements in nums2, copy them over.
+        // (Note: If there are remaining elements in nums1, they are already in place!)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+int main(){
+    vector<int> nums1 = {1, 5, 6, 0, 0, 0}; // nums1 has enough space for nums2
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3; // Number of valid elements in nums1
+    int n = 3; // Number of elements in nums2
+
+    merge(nums1, m, nums2, n);
+
+    cout << "Merged array: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
