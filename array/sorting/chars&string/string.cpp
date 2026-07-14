c2b7ba49e2caee42083d3620e3dc818a034d1ae0
nikhilyt01 <nikhil0808yt@gmail.com>
2026-07-14 16:49:16 +0530

    strings basics

---
+++ array/sorting/chars&string/string.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c[20];
+    cout<< "enter text for usecase of cin.getline :";
+    cin.getline(c,20);
+    cout<<"you entered :"<<c<<endl;
+}
