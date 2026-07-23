cda598b18a611e026557c16e17a64539d653dbc8
nikhilyt01 <nikhil0808yt@gmail.com>
2026-07-23 21:25:32 +0530

    string compression LC=443

---
+++ array/string n char/stringCompression.cpp
@@ -0,0 +1,30 @@
+#include <string>   
+#include <iostream>
+using namespace std;
+//leetcode 443
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int ansIndex=0;
+        int n=chars.size();
+        int i=0;
+        while(i<n){
+            int j=i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++; // if consecutives found i'll moving to next
+            }
+            chars[ansIndex++]=chars[i];
+            int count= j - i;
+            if(count >1){
+                string cnt= to_string(count);
+                for(char ch:cnt){
+                    chars[ansIndex++]=ch;
+                }
+            }
+            i=j;
+
+        }
+        return ansIndex;
+        
+    }
+};
