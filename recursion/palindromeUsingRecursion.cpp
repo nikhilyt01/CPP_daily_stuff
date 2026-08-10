08331506c9e5cea90a46f64d2577a546259fc382
nikhilyt01 <nikhil0808yt@gmail.com>
2026-08-10 22:27:58 +0530

    paindrome using recursion

---
+++ recursion/palindromeUsingRecursion.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+bool pal(string &str,int i ,int j){
+    
+    if(i>j){
+        return true;
+    }
+    if(str[i] != str[j]){
+        return false;
+
+    }
+    else{
+        return pal(str,i+1,j-1);
+    }
+
+
+
+}
+int main(){
+    string name="namana";
+    if(pal(name,0,name.length()-1)){
+        cout<<"palindrome"<<endl;
+    }
+    else{
+        cout<<"not palindrome"<<endl;
+    }
+    
+    
+}
