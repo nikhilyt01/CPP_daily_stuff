dec55aa56e57e9c193e1580d2db48a4f3f31279a
nikhilyt01 <nikhil0808yt@gmail.com>
2026-08-26 16:02:14 +0530

    this keyword

--- oops/intro.cpp
+++ oops/intro.cpp
@@ -12,6 +12,7 @@ class Hero{
     }
     //parameterized constructor
     Hero(int health){
+    cout<<"this->"<<this<<endl;
     this->health=health;
    }   
     void setHealth(int h){
@@ -32,10 +33,11 @@ int main(){
     Hero *b=new Hero(100);
     cout<<"level now is:"<< (*b).level<<endl; // dereferencing pointer *p point to object
     cout<<"health now is:"<< b ->getHealth() ;
-
-   
+    cout<<"------------------------"<<endl;
+    Hero ramesh(18);
+    cout<<"address ramesh same as This value that it point :";
+    cout<<&ramesh<<endl;
     
-
 
 }
 
