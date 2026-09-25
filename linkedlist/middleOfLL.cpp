6332c276d18e4543e1e7fffc6b9e57992bc9b587
nikhilyt01 <nikhil0808yt@gmail.com>
2026-09-25 11:56:51 +0530

    middle of linked l

---
+++ linkedlist/middleOfLL.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+//problem no:876 leetcode
+//random copied struct from leetcode
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+}
+ListNode* middleNode(ListNode* head) {
+        ListNode* temp=head;
+        int cnt=0;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
+        }
+        int mid=(cnt/2 )+1;
+        int i=1;
+        temp=head;
+        while(i<mid){
+          temp=temp->next;
+          i++;
+        }
+        return temp;
+}
